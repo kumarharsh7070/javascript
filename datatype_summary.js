9306974f4dc6ffd8e49cf3d1e1c73c34b372a8fb
kumar harsh <hk995814@gmail.com>
2025-03-12 14:42:12 +0530

    3 video

---
+++ datatype_summary.js
@@ -0,0 +1,11 @@
+// primitive
+
+
+// 7 types: String,number,boolean,null,undefine,symbol,bigint,
+
+const score =100;
+const scorevalue=100.3;
+console.log(score);
+// Reference (Non-primitive)
+
+// Array,object, function
