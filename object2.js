cf207489364f65df9c5807f6be0acb321d519d33
kumar harsh <hk995814@gmail.com>
2025-05-17 12:52:44 +0530

    object

---
+++ object2.js
@@ -0,0 +1,39 @@
+// const tinderUser = new object()      
+// single turn object
+const tinderUser = {}
+tinderUser.id="123abc"
+tinderUser.name= "sammy"
+tinderUser.isLoggedIn=  false
+console.log(tinderUser)
+
+const regularUser={
+     email:"hkumar537@gmail.com",
+     fullname:{
+        userfullname:{
+            firstname:"kumar",
+            lastname:"harsh"
+        }
+     }
+}
+console.log(regularUser.fullname.userfullname.lastname)
+
+const obj1={1:"a",2:"b",}
+const obj2={3:"a",4:"b",}
+const obj3={5:"a",6:"b",}
+
+// const obj4 = Object.assign({},obj1,obj2,obj3)
+
+const obj4={...obj1,...obj2,...obj3}
+console.log(obj4)
+const user=[
+    {
+        email:"hkumar@gmail.com",
+        name:"kumar"
+    },
+    {},
+    {}
+    
+]
+user[1].email
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
