cabf062c86e8f47f4b50762196a10fc38d96afea
kumar harsh <hk995814@gmail.com>
2025-06-09 18:46:34 +0530

    arrow function

--- function.js
+++ function.js
@@ -1,31 +1,77 @@
-// function saynname(){
-//     console.log("H")
-//     console.log("A")
-//     console.log("R")
-//     console.log("S")
-//     console.log("H")
+// // function saynname(){
+// //     console.log("H")
+// //     console.log("A")
+// //     console.log("R")
+// //     console.log("S")
+// //     console.log("H")
+// // }
+// // console.log(saynname())
+
+// // function addtwonumber (num1,num2){
+// //     console.log(num1+num2);
+// // }
+
+// // addtwonumber(2,3)
+
+// // function add (n1,n2){
+// //        let result =n1+n2
+// //        return result
+// // }
+// //  const result = add(3,4)
+// // console.log("result:",result)
+
+// // function loginuser (username="harsh"){
+// //     if(!username){
+// //       console.log("please enter a username")
+// //       return
+// //     }
+// //    return `${username} just logged in`
+// // }
+
+// // console.log(loginuser("kumar"))
+
+// const user ={
+//     username:"hitesh",
+//     prices:199
 // }
-// console.log(saynname())
 
-// function addtwonumber (num1,num2){
-//     console.log(num1+num2);
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.prices}`);
 // }
 
-// addtwonumber(2,3)
+// handleobject(user)
+// handleobject({
+//     username:"sam",
+//     price:399
+// })
 
-// function add (n1,n2){
-//        let result =n1+n2
-//        return result
+// const mynewArray =[200, 400, 100, 600]
+
+// function returnsecondvalue(getArray){
+//     return getArray[3]
 // }
-//  const result = add(3,4)
-// console.log("result:",result)
 
-function loginuser (username="harsh"){
-    if(!username){
-      console.log("please enter a username")
-      return
-    }
-   return `${username} just logged in`
-}
+// console.log(returnsecondvalue([200, 400, 500, 1000]));
 
-console.log(loginuser("kumar"))
+// function square(number) {
+//     return number * number;
+//   }
+//   console.log(square(3))
+// Create an Object
+// const person = {};
+
+// // Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+// console.log(person)
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  
+  delete person.age;
+  console.log(person) 
---
+++ scope.js
@@ -0,0 +1,20 @@
+function one(){
+  const username ="hitesh"
+
+  function two(){
+    const website ="youtube"
+    console.log(username);
+  }
+//   console.log(website)
+  two()
+}
+one()
+
+if(true){
+    const username ="hitesh"
+    if (username==="hitesh"){
+        const website ="youtube"
+        console.log(username + website)
+    }
+
+}
