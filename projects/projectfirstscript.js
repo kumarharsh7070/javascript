5282cf4f45d0cb35aa2fa4bc6f8ff998e9f987f6
kumar harsh <hk995814@gmail.com>
2025-06-17 00:20:05 +0530

    project and dom edit event

--- projects/projectfirstscript.js
+++ projects/projectfirstscript.js
@@ -1,7 +1,32 @@
-const button=document.querySelectorAll(".button");
-const body=document.querySelector(body);
-button.forEach(function (button){
-    button.addEventListener("click", function(e){
-              console.log(e.target)
-    })
-})
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+
+button.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    console.log(e.target);
+    switch (e.target.id) {
+      case "red":
+        body.style.backgroundColor = "grey";
+        document.getElementById("para").innerHTML="Grey"
+        break;
+      case "blue":
+        body.style.backgroundColor = "blue";
+        document.getElementById("para").innerHTML="blue"
+
+        break;
+      case "green":
+        body.style.backgroundColor = "green";
+        document.getElementById("para").innerHTML="green"
+
+        break;
+      case "yellow":
+        body.style.backgroundColor = "yellow";
+        document.getElementById("para").innerHTML="yellow"
+
+        break;
+      default:
+        console.log("No matching color");
+    }
+  });
+});
