5de39e57f197aec7a9bbd1f4dffacd243eb74c41
kumar harsh <hk995814@gmail.com>
2025-06-28 06:42:11 +0530

    currency converter

--- fetch.js
+++ fetch.js
@@ -1,13 +1,23 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-async function fetchPost() {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('Fetch error:', error.message);
-  }
+// async function fetchPost() {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error('Fetch error:', error.message);
+//   }
+// }
+
+// fetchPost();
+const url="https://catfact.ninja/facts";
+const getfact=async ()=>{
+  console.log("get data");
+  let response =await fetch(url);
+  console.log(response);
+  let data = await response.json();
+  console.log(data[0].text);
+  
+  
 }
-
-fetchPost();
