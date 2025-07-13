f03b04b18ccd1c5bcef3c42872da55a63e3ff488
kumar harsh <hk995814@gmail.com>
2025-07-13 22:15:23 +0530

    require

---
+++ node/app.js
@@ -0,0 +1,8 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./3-alternative')
+require('./4-mindgraned')
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
