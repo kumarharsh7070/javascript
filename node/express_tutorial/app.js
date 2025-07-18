4edbd248933bcd75df6f4382b4d8d93e7a7df31b
kumar harsh <hk995814@gmail.com>
2025-07-18 23:58:23 +0530

    get and post with example,streaming enjine and middleware,

--- node/express_tutorial/app.js
+++ node/express_tutorial/app.js
@@ -38,12 +38,45 @@
 // })
 // -------------------------------------------------------
 
- const express = require("express");
- const app = express();
-app.get('/',(req,res)=>{
-    res.json([{name:"harsh"},{name:"rishu"}])
-})
+//  const express = require("express");
+//  const app = express();
+//  app.set('view engine',"ejs")
+// app.use(express.static('./public'))
+ 
+//  app.get("/",(req,res)=>{
+//      res.render("index")
+//  })
+//  app.get("/about",(req,res)=>{
+//      res.render("about")
+//  })
+ 
+ 
+//  app.listen(3000,()=>{
+//      console.log("server start");
+     
+//  })
+ const express = require('express')
+ const app = express()
+ let {people} = require('./data')
+ 
+ app.use(express.static('./public'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'Please provide a name' });
+  }
+
+  const newPerson = { id: people.length + 1, name };
+  people.push(newPerson);
+
+  res.status(201).json({ success: true, person: newPerson });
+});
+
  app.listen(5000,()=>{
-    console.log("server is listening on port 5000...");
+    console.log("server listening on port 50000....");
     
  })
